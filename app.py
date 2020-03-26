1b7c1763eee50010f16ae55650af41c0d5769060
Le Pham <tutin2678@gmail.com>
2020-03-26 20:43:30 +1100

    IN-AUR-01 Detection Stack

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 from aws_cdk import core
 
 from ir_cdk_stacks.ir_cdk_stacks_stack import IrCdkStacksStack
+from ir_cdk_stacks.in_aur_01_detection_stack import InAur01DetectionStack
 
 
 app = core.App()
 IrCdkStacksStack(app, "ir-cdk-stacks")
+InAur01DetectionStack(app, "in-aur-01-detection-stack")
 
 app.synth()
