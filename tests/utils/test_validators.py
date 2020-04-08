e2597dcf65fb84753ce19d6e713d113d815e3229
Le Pham <tutin2678@gmail.com>
2020-04-09 00:32:59 +1000

    Add email validator test

---
+++ tests/utils/test_validators.py
@@ -0,0 +1,25 @@
+import pytest
+from socialist_ir.utils.validators import EmailValidator
+from PyInquirer import ValidationError
+from prompt_toolkit.document import Document
+
+
+def test_email_validator():
+    char_only = Document(text="wrongFormat")
+    digits = Document(text="234234")
+    web = Document(text="abc.com")
+    no_dot_com = Document(text="abc@asd")
+    good = Document(text="test@email.com")
+    with pytest.raises(ValidationError):
+        validator = EmailValidator()
+        validator.validate(char_only)
+    with pytest.raises(ValidationError):
+        validator = EmailValidator()
+        validator.validate(digits)
+    with pytest.raises(ValidationError):
+        validator = EmailValidator()
+        validator.validate(web)
+    with pytest.raises(ValidationError):
+        validator = EmailValidator()
+        validator.validate(no_dot_com)
+    validator.validate(good)
