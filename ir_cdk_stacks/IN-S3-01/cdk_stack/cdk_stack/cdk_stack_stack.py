de5b35aba5891598bc73e1f6f6076916145e25f1
Shubhankar <shubhankar1995@gmail.com>
2020-04-23 01:14:28 +1000

    IN-S3-01 cdk stack for lambda creation

--- ir_cdk_stacks/IN-S3-01/cdk_stack/cdk_stack/cdk_stack_stack.py
+++ ir_cdk_stacks/IN-S3-01/cdk_stack/cdk_stack/cdk_stack_stack.py
@@ -20,9 +20,129 @@ class CdkStackStack(core.Stack):
 
         trail = cloudtrail.Trail(self, "MyAmazingCloudTrail2")
 
-        # trail.add_s3_event_selector(["arn:aws:s3:::socialistir23/"])
-
         trail.add_s3_event_selector(["arn:aws:s3:::socialistir/"],
     		include_management_events=True,
     		read_write_type=cloudtrail.ReadWriteType.WRITE_ONLY
 		)
+
+		# ep = {
+		# 	  "source": [
+		# 	    "aws.s3"
+		# 	  ],
+		# 	  "detail-type": [
+		# 	    "AWS API Call via CloudTrail"
+		# 	  ],
+		# 	  "detail": {
+		# 	    "eventSource": [
+		# 	      "s3.amazonaws.com"
+		# 	    ],
+		# 	    "eventName": [
+		# 	      "ListObjects",
+		# 	      "ListObjectVersions",
+		# 	      "PutObject",
+		# 	      "GetObject",
+		# 	      "HeadObject",
+		# 	      "CopyObject",
+		# 	      "GetObjectAcl",
+		# 	      "PutObjectAcl",
+		# 	      "CreateMultipartUpload",
+		# 	      "ListParts",
+		# 	      "UploadPart",
+		# 	      "CompleteMultipartUpload",
+		# 	      "AbortMultipartUpload",
+		# 	      "UploadPartCopy",
+		# 	      "RestoreObject",
+		# 	      "DeleteObject",
+		# 	      "DeleteObjects",
+		# 	      "GetObjectTorrent",
+		# 	      "SelectObjectContent",
+		# 	      "PutObjectLockRetention",
+		# 	      "PutObjectLockLegalHold",
+		# 	      "GetObjectLockRetention",
+		# 	      "GetObjectLockLegalHold"
+		# 	    ],
+		# 	    "requestParameters": {
+		# 	      "bucketName": [
+		# 	        "socialistir-dev"
+		# 	      ]
+		# 	    }
+		# 	  }
+		# 	}
+
+  #       rule = events.Rule(self,
+  #                          "cdkRule",
+  #                          description= 'Rule created by CDK',
+  #                          enabled= True,
+  #                          rule_name= "rulebycdk",
+  #                          event_pattern= ep)
+
+        
+        lambda_dir_path = "D:\\OneDrive - UNSW\\Term 4\\AWS\\Project\\IR-CDK-Stacks\\ir_cdk_stacks\\IN-S3-01\\response_lambda"
+        response_lambda = _lambda.Function(
+            self,
+            "CDK-S3WriteIR-TEST",
+            runtime=_lambda.Runtime.PYTHON_3_8,
+            handler="lambda_function.lambda_handler",
+            code=_lambda.Code.from_asset(lambda_dir_path),
+            function_name="CDK-S3WriteIR-TEST"
+        )
+
+        response_lambda.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=[
+                        "iam:*",
+                        "organizations:DescribeAccount",
+                        "organizations:DescribeOrganization",
+                        "organizations:DescribeOrganizationalUnit",
+                        "organizations:DescribePolicy",
+                        "organizations:ListChildren",
+                        "organizations:ListParents",
+                        "organizations:ListPoliciesForTarget",
+                        "organizations:ListRoots",
+                        "organizations:ListPolicies",
+                        "organizations:ListTargetsForPolicy"
+                    ],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+
+        response_lambda.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=[
+                        "iam:*",
+                        "organizations:DescribeAccount",
+                        "organizations:DescribeOrganization",
+                        "organizations:DescribeOrganizationalUnit",
+                        "organizations:DescribePolicy",
+                        "organizations:ListChildren",
+                        "organizations:ListParents",
+                        "organizations:ListPoliciesForTarget",
+                        "organizations:ListRoots",
+                        "organizations:ListPolicies",
+                        "organizations:ListTargetsForPolicy"
+                    ],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+
+        response_lambda.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=[
+                    	"s3:*"
+                    ],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+
+        response_lambda.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=[
+		                "sns:*"
+		            ],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
