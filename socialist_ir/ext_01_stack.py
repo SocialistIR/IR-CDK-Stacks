105941131fcdef8cc42822588cf3e0ebf26cba40
ecl1ipse <shanildesilva@gmail.com>
2020-04-27 21:59:15 +1000

    couldn't do automated testing on this, requires to much time and access

--- socialist_ir/ext_01_stack.py
+++ socialist_ir/ext_01_stack.py
@@ -2,6 +2,7 @@ from PyInquirer import prompt
 from socialist_ir.cdk_menu import StackMenu
 from socialist_ir.utils.validators import (
     ApiArnValidator,
+    YesOrNoValidator,
 )
 from socialist_ir.config import Config
 
@@ -13,6 +14,13 @@ class Ext01Stack(StackMenu):
         required_variables=["api_arn"],
     ):
         super().__init__(name=name, required_variables=required_variables)
+
+    def test(self) -> None:
+        print("Automated testing not doable.\n Manual testing procedure:\n1. Enter <script>alert(123);</script> as input to any API endpoint.")
+        print("2. Wait 5 minutes")
+        print("3. Check Ext06ResponseIpSet to see if it contains your IP address. If it doesn't depoyment has failed")
+        print("4. If it does, wait for an hour and see if it gone")
+        print("5. If it has disappeared, deployment was succesful. Otherwise it failed")
 
     def setup(self):
         # Prompt required variables
