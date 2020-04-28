5fd4c0e23c864ca3c113ea9d3ba8b49cd76166e4
Shubhankar <shubhankar1995@gmail.com>
2020-04-29 01:09:47 +1000

    cli code modified app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from aws_cdk import core
 
 from ir_cdk_stacks.ir_cdk_stacks_stack import IrCdkStacksStack
 from ir_cdk_stacks.in_aur_01_stack import InAur01Stack
+from ir_cdk_stacks.In_S3_01_Prod_Stack import InS301StackProd
 from socialist_ir.config import Config
 
 config = Config.get_config()
@@ -14,7 +15,8 @@ REGION = config.get("main", "region")
 env_US = core.Environment(account=ACCOUNT, region=REGION)
 
 app = core.App()
-IrCdkStacksStack(app, "ir-cdk-stacks", env=env_US)
-InAur01Stack(app, "in-aur-01-stack", env=env_US)
+# IrCdkStacksStack(app, "ir-cdk-stacks", env=env_US)
+# InAur01Stack(app, "in-aur-01-stack", env=env_US)
+InS301StackProd(app, "in-s3-01-stack", env=env_US)
 
 app.synth()
