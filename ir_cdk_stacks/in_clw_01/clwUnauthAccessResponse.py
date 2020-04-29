ffdb418a804c01e69dc05e6827f3ac1948339acc
skaraninder <39990752+skaraninder@users.noreply.github.com>
2020-04-30 08:54:24 +1000

    lambda changes

--- ir_cdk_stacks/in_clw_01/clwUnauthAccessResponse.py
+++ ir_cdk_stacks/in_clw_01/clwUnauthAccessResponse.py
@@ -1,42 +1,104 @@
 import json
 import boto3
+import urllib3
+
+iam = boto3.client('iam')
+sns = boto3.client('sns')
+clwDenyPolicy = 'arn:aws:iam::544820149332:policy/ClWDenyAccess1'
+clwDenyPolicy2 = 'arn:aws:iam::544820149332:policy/ClWDenyAccess2'
+clwDenyPolicyName1 = 'ClWDenyAccess1'
+clwDenyPolicyName2 = 'ClWDenyAccess2'
+clwGroupName = 'CLWAccess'
+clwNotificationTopicArn = 'arn:aws:sns:us-east-1:544820149332:CDKCLWAccess'
+
+http = urllib3.PoolManager()
+
+
+def hasValidGroup(userName):
+    response = iam.get_group(GroupName=clwGroupName)
+
+    groupUsernames = []
+
+    for user in response['Users']:
+        groupUsernames.append(user['UserName'])
+
+    # groupUsernames.remove('Karan')
+
+    if userName in groupUsernames:
+        return True
+    else:
+        return False
+
+
+def hasDenyPolicy(userName):
+    inline_user_policies = iam.list_attached_user_policies(UserName=userName)['AttachedPolicies']
+
+    print('Check Deny Policy')
+
+    found = False
+    for policy in inline_user_policies:
+        if policy['PolicyName'] == clwDenyPolicyName1 or policy['PolicyName'] == clwDenyPolicyName2:
+            found = True
+            break
+
+    return found
+
+
+def sendNotification(message):
+    print('sending notification')
+    response = sns.publish(
+        TopicArn=clwNotificationTopicArn,
+        Message=message,
+        Subject='Access to CloudWatch',
+        MessageStructure='string'
+    )
+
+
+def sendSlackNotification(message):
+    message = "IN-CLW-01 Unauthorised CloudWatch Access:\n" + message
+    # webhook_url = os.environ["webhook_url"]
+    webhook_url = "https://hooks.slack.com/services/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"
+    slack_message = {"channel": "ir-cdk-stacks", "text": message}
+    encoded_data = json.dumps(slack_message).encode("utf-8")
+    response = http.request(
+        "POST",
+        webhook_url,
+        body=encoded_data,
+        headers={"Content-Type": "application/json"},
+    )
 
 
 def lambda_handler(event, context):
-    clwDenyPolicy = 'arn:aws:iam::544820149332:policy/denyIAM'
-    clwDenyPolicy2 = 'arn:aws:iam::544820149332:policy/DenyIAM2'
-
-    groupName = 'testgroupclw'
-    iam = boto3.client('iam')
-    sns = boto3.client('sns')
-    print(event)
-    print(context)
-    # TODO implement
     userName = event['detail']['userIdentity']['userName']
     userType = event['detail']['userIdentity']['type']
-    # sourceIPAddress = event["detail"]['sourceIPAddress']
-    # eventTime = event["detail"]["eventTime"]
-    # userAgent = event["detail"]["userAgent"]
-    # eventName = event["detail"]["eventName"]
+    sourceIPAddress = event["detail"]['sourceIPAddress']
+    eventTime = event["detail"]["eventTime"]
+    userAgent = event["detail"]["userAgent"]
+    eventName = event["detail"]["eventName"]
+    message = f'IN_CLW_01: {userName} tried to perform {eventName} on Cloudwatch at time {eventTime} from userAgent {userAgent} with IP {sourceIPAddress}. '
 
-    response = iam.get_group(GroupName='testgroupclw')
-    print(response['Group']['GroupName'])
-    groupUsernames = []
-    for user in response['Users']:
-        groupUsernames.append(user['UserName'])
-    groupUsernames.remove('Karan')
-    print(groupUsernames)
-    # "type": "IAMUser",
-    if userName not in groupUsernames and userType == "IAMUser":
-        response = iam.attach_user_policy(UserName=userName, PolicyArn=clwDenyPolicy)
-        response = iam.attach_user_policy(UserName=userName, PolicyArn=clwDenyPolicy2)
+    validUser = hasValidGroup(userName)
 
-    print('attach policy')
-    # return response['ResponseMetadata']['HTTPStatusCode'] == 200
+    if hasDenyPolicy(userName):
 
-    # print(userName)
-    # print("UserName: {0}\nCreateDate: {1}\n".format(user['UserName'], user['CreateDate']))
+        message = message + ' CloudWatchDeny Policy is already attached to deny access to CloudWatch.'
+        sendNotification(message)
+
+    else:
+        if userType == "IAMUser" and not validUser:
+
+            response = iam.attach_user_policy(UserName=userName, PolicyArn=clwDenyPolicy)
+            response = iam.attach_user_policy(UserName=userName, PolicyArn=clwDenyPolicy2)
+
+            message = message + ' CloudWatchDeny Policy attached to deny access to CloudWatch.'
+            sendNotification(message)
+
+        else:
+
+            message = message + ' Access Granted : CloudWatch Access allowed.'
+            sendNotification(message)
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda.hello fellow!')
+        'body': json.dumps('Hello from CloudWatch Lambda!')
     }
