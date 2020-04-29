281aca38de37d54d3bbd71e106f813165dfcbb06
henryncepu <51872921+henryncepu@users.noreply.github.com>
2020-04-29 17:22:15 +1000

    Update IR-IAM-01-Response.py

--- ir_cdk_stacks/IR_IAM_01/IR-IAM-01-Response.py
+++ ir_cdk_stacks/IR_IAM_01/IR-IAM-01-Response.py
@@ -1,67 +1,82 @@
 import json
 import boto3
+import urllib3
 
 iam = boto3.client('iam')
 sns = boto3.client('sns')
 IAM_Allow_ModifyPolicyArn = 'arn:aws:iam::544820149332:policy/IAM_Allow_modify'
-notificationTopicArn = 'arn:aws:sns:us-east-1:544820149332:send-email-no-SNS'
+IAM_Deny_ModifyPolicyArn = 'arn:aws:iam::544820149332:policy/IAM_Deny_modify'
+IAM_Deny_ModifyPolic = 'IAM_Deny_modify'
+notificationTopicArn = 'arn:aws:sns:us-east-1:544820149332:IAM-test-sns'
 IAM_modifyGroupName = 'IAM_Allow_Modify'
 
-def hasValidPolicy(userName):    
-    inline_user_policies = iam.list_attached_user_policies(UserName=userName)['AttachedPolicies']
+# http = urllib3.PoolManager()
+
+
+def hasValidGroup(userName):
     inline_user_groups = iam.list_groups_for_user(UserName=userName)
 
-    print('Check ValidPolicy')
-    
+    print('Check Valid Group')
+
     found = False
-    
     for group in inline_user_groups['Groups']:
         if group['GroupName'] == IAM_modifyGroupName:
             found = True
+
     return found
-    
+
+
 def attachDenyPolicy(userName, denyPolicy):
     response = iam.attach_user_policy(
-        UserName = userName,
-        PolicyArn = denyPolicy
+        UserName=userName,
+        PolicyArn=denyPolicy
     )
-    
-    print('attach Deny policy')
+
+    print('attaching Deny policy')
     return response['ResponseMetadata']['HTTPStatusCode'] == 200
 
-def sendNotification( userName, sourceIPAdrress, eventName, eventTime, userAgent):
-   
-    message = 'We found:'+userName+'pretending to modify{'+ eventName +'} on IAM '+'at time [' + eventTime + '] and the sourceIP is' + sourceIPAdrress
+
+def sendNotification(message):
     print('sending notification')
     response = sns.publish(
-        TopicArn= notificationTopicArn,
-        Message= message,
-        Subject='Unauthorised IAM to Modify',
+        TopicArn=notificationTopicArn,
+        Message=message,
+        Subject='Unauthorised access to modify IAM',
         MessageStructure='string'
     )
-    
-    
-    
+
+
 def lambda_handler(event, context):
     print('lambda_handler')
-    
+
     userName = event['detail']['userIdentity']['userName']
     sourceIPAddress = event["detail"]['sourceIPAddress']
     eventTime = event["detail"]["eventTime"]
+    userAgent = event["detail"]["userAgent"]
     eventName = event["detail"]["eventName"]
     
-    if not hasValidPolicy(userName) :# if not be assigned a valid policy,trigger the SNS 
+    #print(event)
+    
+    message = f'username: {userName} tried to perform {eventName} on CLoudTrail at time {eventTime} from userAgent {userAgent} with IP {sourceIPAddress}. '
+    print('MMMM',message)
+    
+    if not hasValidGroup(userName):
+        # 1. Attach Deny Policy
         maxReAttempts = 3
-        
-        while (maxReAttempts > 0) :
+
+        while (maxReAttempts > 0):
             if attachDenyPolicy(userName, IAM_Deny_ModifyPolicyArn) is False:
                 maxReAttempts -= 1
             else:
                 break
-     
-        sendNotification(userName , sourceIPAddress , eventTime , eventName)
-    
+
+        # 2. Inform developers with source IP Address
+
+        message = message + ' IAM-Deny-modify Policy attached to deny access to IAM.'
+        sendNotification(message)
+
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from IAM_Modify_Lambda!')
+        'body': json.dumps('Hello from CLT_!')
     }
+
