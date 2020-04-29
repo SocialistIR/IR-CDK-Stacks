731ec617f0749b4c5fb6fd0a6c122817bc09fd8b
skaraninder <39990752+skaraninder@users.noreply.github.com>
2020-04-29 15:35:52 +1000

    Revert "Revert "lambda policy changes""
    This reverts commit fa120eabdef5b86cbf24c5b2b781774e30b9cd49.

--- ir_cdk_stacks/in_clw_01/clwUnauthAccessResponse.py
+++ ir_cdk_stacks/in_clw_01/clwUnauthAccessResponse.py
@@ -1,14 +1,31 @@
 import json
 import boto3
+import urllib3
 
+iam = boto3.client('iam')
+sns = boto3.client('sns')
+clwDenyPolicy='arn:aws:iam::544820149332:policy/ClWDenyAccess1'
+clwDenyPolicy2='arn:aws:iam::544820149332:policy/ClWDenyAccess2'
+clwGroupName='testgroupclw'
+clwNotificationTopicArn = 'arn:aws:sns:us-east-1:544820149332:CDKCLWAccess'
+
+http = urllib3.PoolManager()
 
 def lambda_handler(event, context):
-    clwDenyPolicy = 'arn:aws:iam::544820149332:policy/denyIAM'
-    clwDenyPolicy2 = 'arn:aws:iam::544820149332:policy/DenyIAM2'
+    
+    userName = event['detail']['userIdentity']['userName']
+    userType = event['detail']['userIdentity']['type']
+    
+    #if userType=="IAMUser":
+        
+    # sourceIPAddress = event["detail"]['sourceIPAddress']
+    # eventTime = event["detail"]["eventTime"]
+    # userAgent = event["detail"]["userAgent"]
+    # eventName = event["detail"]["eventName"]
+    # message = f'IN_CLW_01: {userName} tried to perform {eventName} on Cloudwatch at time {eventTime} from userAgent {userAgent} with IP {sourceIPAddress}. '
 
-    groupName = 'testgroupclw'
-    iam = boto3.client('iam')
-    sns = boto3.client('sns')
+    
+    
     print(event)
     print(context)
     # TODO implement
@@ -18,24 +35,24 @@ def lambda_handler(event, context):
     # eventTime = event["detail"]["eventTime"]
     # userAgent = event["detail"]["userAgent"]
     # eventName = event["detail"]["eventName"]
-
+    
     response = iam.get_group(GroupName='testgroupclw')
     print(response['Group']['GroupName'])
-    groupUsernames = []
+    groupUsernames=[]
     for user in response['Users']:
         groupUsernames.append(user['UserName'])
     groupUsernames.remove('Karan')
     print(groupUsernames)
     # "type": "IAMUser",
-    if userName not in groupUsernames and userType == "IAMUser":
-        response = iam.attach_user_policy(UserName=userName, PolicyArn=clwDenyPolicy)
-        response = iam.attach_user_policy(UserName=userName, PolicyArn=clwDenyPolicy2)
-
+    if userName not in groupUsernames and userType=="IAMUser":
+        response = iam.attach_user_policy(UserName = userName, PolicyArn = clwDenyPolicy)
+        response = iam.attach_user_policy(UserName = userName, PolicyArn = clwDenyPolicy2)
+    
     print('attach policy')
-    # return response['ResponseMetadata']['HTTPStatusCode'] == 200
-
-    # print(userName)
-    # print("UserName: {0}\nCreateDate: {1}\n".format(user['UserName'], user['CreateDate']))
+    #return response['ResponseMetadata']['HTTPStatusCode'] == 200
+    
+    #print(userName)
+        # print("UserName: {0}\nCreateDate: {1}\n".format(user['UserName'], user['CreateDate']))
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda.hello fellow!')
