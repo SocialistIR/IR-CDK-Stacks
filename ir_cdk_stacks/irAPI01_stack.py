91cf3b60423d8dd5361832bb4b5b342940459c1c
korupalli <45527890+korupalli@users.noreply.github.com>
2020-05-07 12:06:27 +1000

    Create irAPI01_stack.py

---
+++ ir_cdk_stacks/irAPI01_stack.py
@@ -0,0 +1,84 @@
+import boto3
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_iam as iam,
+    aws_s3 as _s3,
+    aws_s3_notifications,
+    core
+)
+
+class InApi01Stack(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        
+        s3 = _s3.Bucket(self, "testbuckforall")
+        print(s3)
+        
+        cf = boto3.client('cloudformation')
+        response = cf.describe_stacks(StackName='IR-API-01-Test')
+        
+        Outputs = {}
+        for e in response['Stacks'][0]['Outputs']:
+                Outputs[e['OutputKey']] = e['OutputValue']
+        print(Outputs)
+        function = _lambda.Function(self, "lambda_function",
+                                    runtime=_lambda.Runtime.PYTHON_2_7, memory_size = 512, #timeout=core.Duration(120),
+                                    handler="parser.lambda_handler",
+                                    code=_lambda.Code.asset("./lambda"))
+                                    
+        function.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=["s3:*"],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+        function.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=["waf-regional:UpdateIPSet","waf-regional:GetIPSet","waf-regional:GetChangeToken"],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+        function.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=["logs:*"],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+        function.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=["cloudformation:DescribeStacks"],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+        function.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=["SNS:Publish"],
+                    effect=iam.Effect.ALLOW,
+                    resources=["arn:aws:sns:us-east-1:544820149332:IN-API-01-IPBlocked"],
+                )
+            )
+        function.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=["cloudwatch:PutMetricData"],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+        function.add_to_role_policy(
+                iam.PolicyStatement(
+                    actions=["lambda:*"],
+                    effect=iam.Effect.ALLOW,
+                    resources=["*"],
+                )
+            )
+
+        # create s3 notification for lambda function
+        notification = aws_s3_notifications.LambdaDestination(function)
+
+        # assign notification for the s3 event type (ex: OBJECT_CREATED)
+        s3.add_event_notification(_s3.EventType.OBJECT_CREATED, notification)
